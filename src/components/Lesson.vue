08715e683af7c95f8cb27bddb46577da9c18d194
Anton Tymczenko <tymczenko@protonmail.com>
2018-02-22 16:00:01 +0200

    feat: Lesson initial commit

--- src/components/Lesson.vue
+++ src/components/Lesson.vue
@@ -1,17 +1,15 @@
 <template>
 <div>
-<h1>This is {{ label }} placeholder</h1>
-<router-link to="/">Home</router-link>
+  <router-link to="/">Home</router-link>
+  <h1>{{ label }}</h1>
 </div>
 </template>
 
 <script>
 export default {
   name: 'Lesson',
-  data () {
-    return {
-      label: 'Lesson'
-    }
-  }
+  data: () => ({
+    label: 'Lesson'
+  })
 }
 </script>
