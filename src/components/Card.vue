64bb1f156742fade1a78c186d8c3efdc37f54377
Anton Tymczenko <tymczenko@protonmail.com>
2018-02-22 17:34:59 +0200

    feat: Style Card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -22,4 +22,40 @@ export default {
 </script>
 
 <style scoped>
+.flashcard {
+  margin: 0 auto;
+  width: 200px;
+  height: 200px;
+  position: relative;
+  border-width: 3px;
+  border-style: solid;
+  border-radius: 20px;
+}
+
+.question {
+  border-color: #39f;
+  font-size: 2rem;
+  font-weight: 700;
+}
+
+.answer {
+  border-color: #aaa;
+  font-size: 1.4rem;
+}
+
+.flashcard * {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.flashcard .content {
+  line-height: 200px;
+}
+
+.flashcard .overlay {
+  z-index: 10;
+}
 </style>
