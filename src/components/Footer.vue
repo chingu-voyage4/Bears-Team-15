57e5f17ee7ef9a2b82976c97ee8047f1d195bf0f
Anton Tymczenko <tymczenko@protonmail.com>
2018-02-22 03:45:42 +0200

    feat: Add copyright with a link to our repo

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,19 @@
 <template>
 <div>
+  © {{ currentYear }}
+  <a href="https://github.com/chingu-voyage4/Bears-Team-15#readme" target="_blank">Chingu Voyage-4, team Bears-15</a>
 </div>
 </template>
 
 <script>
 export default {
+  data: () => ({
+  }),
+  computed: {
+    currentYear () {
+      return (new Date()).getFullYear()
+    }
+  }
 }
 </script>
 
