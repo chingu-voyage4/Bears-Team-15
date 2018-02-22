f7f763c45f7d304d1c8b661e4586f16aff214c8b
Anton Tymczenko <tymczenko@protonmail.com>
2018-02-22 05:10:50 +0200

    feat: Remake footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,21 @@
 <template>
-<div>
-  © {{ currentYear }}
-  <a href="https://github.com/chingu-voyage4/Bears-Team-15#readme" target="_blank">Chingu Voyage-4, team Bears-15</a>
-</div>
+<footer>
+  <div class="content">
+    <span class="pull-left">
+      © {{ currentYear }}
+      <a href="https://github.com/chingu-voyage4/Bears-Team-15#readme" target="_blank">Chingu Voyage-4, team Bears-15</a>
+    </span>
+    <span class="pull-right">
+      v {{ version }}
+    </span>
+  </div>
+</footer>
 </template>
 
 <script>
 export default {
   data: () => ({
+    version: '0.1'
   }),
   computed: {
     currentYear () {
