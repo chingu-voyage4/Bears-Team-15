91e62221228c31ecebfd4789d511ff22e87a6958
Anton Tymczenko <tymczenko@protonmail.com>
2018-02-22 17:27:22 +0200

    feat: Import Card component

--- src/components/Lesson.vue
+++ src/components/Lesson.vue
@@ -2,12 +2,22 @@
 <div>
   <router-link to="/">Home</router-link>
   <h1>{{ collection.collectionName }}</h1>
+  <div
+  >
+    <app-card
+    > {{ card }} </app-card>
+  </div>
 </div>
 </template>
 
 <script>
+import Card from '@/components/Card'
+
 export default {
   name: 'Lesson',
+  components: {
+    appCard: Card
+  },
   data: () => ({
     collection: {
       collectionName: 'Italian words',
