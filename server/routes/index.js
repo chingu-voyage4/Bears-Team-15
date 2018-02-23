089c26e9e17696e938f6c2814fef0e1a8bc58472
Anton Tymczenko <tymczenko@protonmail.com>
2018-02-23 02:40:34 +0200

    feat: API READ Collection dummy route

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,12 @@ router.get('/', (req, res) => {
   res.status(200).sendFile(path.resolve(__dirname, '../../dist/index.html'))
 })
 
+const {Collection} = require('../models')
+
+router.get('/collection', (req, res) => {
+  res.status(200).send('Collection API route')
+})
+
 router.get('*', (req, res) => {
   res.status(404).send('Not found')
 })
