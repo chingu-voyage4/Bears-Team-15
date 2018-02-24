27386bbaad578fac54df771e4c7c25f69f5153ea
Anton Tymczenko <tymczenko@protonmail.com>
2018-02-24 20:03:57 +0200

    refactor: Move Footer's style to root Component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -24,36 +24,3 @@ export default {
   }
 }
 </script>
-
-<style>
-body, .homepage {
-  height: 100vh;
-  margin: 0;
-  display: flex;
-  flex-direction: column;
-}
-
-footer {
-  min-height: 50px;
-  height: 10vh;
-  width: 100%;
-  background: #ddd;
-  margin-top: auto;
-  display: table;
-}
-
-footer .content {
-  display: table-cell;
-  vertical-align: middle;
-  line-height: 50px;
-  padding: 0 30px;
-}
-
-.pull-left {
-  float: left;
-}
-
-.pull-right {
-  float: right;
-}
-</style>
