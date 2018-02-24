111cde42e537a8b72a19ab3136303052a4dc0bdf
Anton Tymczenko <tymczenko@protonmail.com>
2018-02-24 20:06:16 +0200

    refactor: Move Dashboard's style to root Component

--- src/App.vue
+++ src/App.vue
@@ -49,4 +49,35 @@ footer .content {
 .pull-right {
   float: right;
 }
+
+.dashboard {
+  width: 80%;
+  height: 400px;
+  margin: 0 auto;
+  display: flex;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+.lesson {
+  width: 140px;
+  height: 80px;
+  margin: 0 auto;
+  margin-top: 80px;
+  margin-bottom: 40px;
+  padding: 60px 0;
+  background: rgb(240,240,240);
+  border-radius: 10px;
+  box-shadow: 2px 5px 0px rgb(200,200,200),
+    4px 10px 0px rgb(180,180,180),
+    12px 15px 10px rgba(0,0,0,0.2);
+}
+
+.spacer {
+  width: 33%;
+  margin: 0 auto;
+}
 </style>
