421c1629f17f3b42c651ba34469d9de9c5828355
Anton Tymczenko <tymczenko@protonmail.com>
2018-03-01 18:22:24 +0200

    feat: Direct mutation

--- src/components/CollectionEdit.vue
+++ src/components/CollectionEdit.vue
@@ -1,10 +1,29 @@
 <template>
-<h1>collection edit</h1>
+<div class="container">
+  <div
+    class="card"
+    v-for="(card, index) in collection.items" :key="index"
+  >
+    <input type="text" v-model="card.q">
+    <input type="text" v-model="card.a">
+  </div>
+</div>
 </template>
 
 <script>
 export default {
   props: ['id'],
+  computed: {
+    collection () {
+      if (this.id === undefined) {
+        return {
+          collectionName: '',
+          items: [{ q: '', a: '' }]
+        }
+      }
+      return {...this.$store.state.collections[this.id]}
+    }
+  },
 }
 </script>
 
