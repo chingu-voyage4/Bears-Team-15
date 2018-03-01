b93702f649c66cf5a123c9f5f48ebc0fb4d7a985
Anton Tymczenko <tymczenko@protonmail.com>
2018-03-01 16:53:58 +0200

    feat: Add styling to navigation links. resolves #52

--- src/components/Collection.vue
+++ src/components/Collection.vue
@@ -44,4 +44,25 @@ export default {
 </script>
 
 <style scoped>
+nav {
+  margin-bottom: 40px;
+}
+
+nav ul {
+  list-style: none;
+}
+
+nav li {
+  display: inline;
+  margin: 0 20px;
+}
+
+nav li a {
+  font-size: 1.2rem;
+  padding: 10px;
+}
+
+nav .router-link-exact-active {
+  background: #0bf;
+}
 </style>
