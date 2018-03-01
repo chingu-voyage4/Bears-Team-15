38a5123daeb16d163056f20cfa6d5a6261356c2f
Anton Tymczenko <tymczenko@protonmail.com>
2018-03-01 16:45:43 +0200

    refactor: Move styling of Dashboard from App to the actual component

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -29,3 +29,29 @@ export default {
   })
 }
 </script>
+
+<style scoped>
+.dashboard {
+  width: 80%;
+  height: 400px;
+  margin: 0 auto;
+  display: flex;
+}
+
+.deck {
+  width: 140px;
+  height: 80px;
+  margin: 40px;
+  padding: 60px 0;
+  background: rgb(240, 240, 240);
+  border-radius: 10px;
+  box-shadow: 2px 5px 0px rgb(200, 200, 200),
+    4px 10px 0px rgb(180, 180, 180),
+    12px 15px 10px rgba(0, 0, 0, 0.2);
+}
+
+.spacer {
+  width: 33%;
+  margin: 0 auto;
+}
+</style>
