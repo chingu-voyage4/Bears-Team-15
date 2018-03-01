acb151634068b6147a91e0305e780dbc92a3e196
Anton Tymczenko <tymczenko@protonmail.com>
2018-03-01 04:42:42 +0200

    fix: Remove router-link from collecitonName in Lesson component

--- src/components/Lesson.vue
+++ src/components/Lesson.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
   <h1>
-    <router-link to="/collection">{{ collection.collectionName }}</router-link>
+    {{ collection.collectionName }}
   </h1>
   <div
     @click.stop="nextCard"
