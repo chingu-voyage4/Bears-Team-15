2150661e7e633a878c17caec8bcc4182e311165f
Anton Tymczenko <tymczenko@protonmail.com>
2018-03-08 18:51:43 +0200

    fix: Add fallback 'development' mode

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const {NODE_ENV} = process.env
+const NODE_ENV = process.env.NODE_ENV || 'development'
 
 // application-level middleware:
 const bodyParser = require('body-parser')
