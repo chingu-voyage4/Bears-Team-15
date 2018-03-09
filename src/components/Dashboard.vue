affbbf095d1276e2c0e684e31daf0b7fb6762c9e
Anton Tymczenko <tymczenko@protonmail.com>
2018-03-09 13:58:20 +0200

    feat: Use Nav Tabs in Dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -17,17 +17,32 @@
         </div>
       </router-link>
     </div>
+  <app-nav-tabs :navItems="navItems"></app-nav-tabs>
   </div>
 </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import NavTabs from '@/components/NavTabs'
 
 export default {
   name: "Dashboard",
+  components: {
+    appNavTabs: NavTabs,
+  },
   data: () => ({
     greeting: 'Welcome to Bears-15 Cards!',
+    navItems: [
+      {
+        display: 'My decks',
+        route: { name: 'home' }
+      },
+      {
+        display: 'Discover',
+        route: { name: 'public' }
+      },
+    ]
   }),
   computed: mapState(['collections']),
 }
