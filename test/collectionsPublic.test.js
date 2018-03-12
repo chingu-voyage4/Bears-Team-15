612efd47dac9fd55f4b5b43725e44a2b312a315e
Anton Tymczenko <tymczenko@protonmail.com>
2018-03-12 19:46:51 +0200

    test: Check for proper structure of received object

--- test/collectionsPublic.test.js
+++ test/collectionsPublic.test.js
@@ -14,5 +14,31 @@ beforeAll(async () => {
 afterAll(server.close())
 
 describe('GET `/collections/public`', () => {
+  it('should respond with collections', () =>
+    chai.request(app)
+      .get('/collections/public')
+      .then(res => {
+        const received = res.body
 
+        // to have proper structure:
+        expect(received).toBeDefined()
+        expect(received).toHaveProperty('collections')
+        expect(received.collections).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              collectionName: expect.any(String),
+              id: expect.any(String),
+              // N.B! "id", not "_id"
+              items: expect.arrayContaining([
+                expect.objectContaining({
+                  q: expect.any(String),
+                  a: expect.any(String),
+                  _id: expect.any(String),
+                }),
+              ]),
+            })
+          ])
+        )
+      })
+  )
 })
