5e0b9743c27301b04026717932082107798a7861
Anton Tymczenko <tymczenko@protonmail.com>
2018-03-12 19:38:00 +0200

    chore: Use 'chai' and 'chai-http' for making requests

--- test/collectionsPublic.test.js
+++ test/collectionsPublic.test.js
@@ -3,6 +3,11 @@ import app from '../server/app'
 import connect from '../server/mongoose'
 const MONGODB_URI = process.env.MONGODB_URI + '-test'
 
+import chai from 'chai'
+import chaiHttp from 'chai-http'
+
+chai.use(chaiHttp)
+
 describe('GET `/collections/public`', () => {
   beforeEach(() => {
   })
