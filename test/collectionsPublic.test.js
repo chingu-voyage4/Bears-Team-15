505a9c46183afddf0c08537a9bdd7429b03acf74
Anton Tymczenko <tymczenko@protonmail.com>
2018-03-12 19:43:40 +0200

    test: Seed before all tests

--- test/collectionsPublic.test.js
+++ test/collectionsPublic.test.js
@@ -1,10 +1,15 @@
 import { app, server } from '../server/app'
+import * as seed from '../server/seed/modules'
 
 import chai from 'chai'
 import chaiHttp from 'chai-http'
 
 chai.use(chaiHttp)
 
+beforeAll(async () => {
+  await seed.resetAllCollections()
+  await seed.populateCollections()
+})
 
 afterAll(server.close())
 
