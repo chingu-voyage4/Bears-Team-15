a56449901ff96d3491d9ea886ab90ce71834eee9
Anton Tymczenko <tymczenko@protonmail.com>
2018-03-17 03:50:52 +0200

    feat: Auth API routes
    `/register` and `/login`
    
    Both routes take { login, password } as input.
    
    Both routes respond with { login, _id } (this is defined in user's
    `toJSON` method)
    
    Every response in case of error is defined in
    `test/authentication.test.js`

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,6 +9,54 @@ router.get('/', (req, res) => {
 
 const { User, Collection, Card } = require('../models')
 
+router.post('/register', (req, res) => {
+  const { login, password } = req.body
+  User.create({ login, password })
+    .then(user => {
+      res.status(200).send(user)
+    })
+    .catch(err => {
+      let statusCode = 400
+      let msg = errors.registration.common
+      if (err.code == 11000) {
+        msg = errors.registration.duplicate
+      } else if (err.errors) {
+        const e = err.errors
+        if (e.login && e.login.kind === 'required') {
+          msg = errors.registration.empty.login
+        } else if (e.password && e.password.kind === 'required') {
+          msg = errors.registration.empty.password
+        }
+      } else {
+        console.error(err)
+        statusCode = 500
+      }
+      res.status(statusCode).send(msg)
+    })
+})
+
+router.post('/login', async (req, res) => {
+  const { login, password } = req.body
+  let msg = ''
+  let statusCode = 403
+  try {
+    if (!login || !password) throw 'empty'
+    const user = await User.findByCredentials(login, password)
+    res.status(200).send(user)
+  } catch (err) {
+    if (err === 'empty') {
+      msg = errors.login.common
+    } else if (err === 'wrong') {
+      msg = errors.login.wrongCredentials
+    } else {
+      console.error(err)
+      msg = errors.worstScenario
+      statusCode = 500
+    }
+    res.status(statusCode).send(msg)
+  }
+})
+
 router.get('/collection', (req, res) => {
   res.status(200).send('Collection API route')
 })
