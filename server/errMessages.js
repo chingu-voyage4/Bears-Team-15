2faf1f9377776c2762e992a8d887c6c31366a7e9
Anton Tymczenko <tymczenko@protonmail.com>
2018-03-17 03:42:12 +0200

    feat: Create error messages file

---
+++ server/errMessages.js
@@ -0,0 +1,15 @@
+export default {
+  registration: {
+    common: 'Failed to register user',
+    duplicate: 'User is already registered',
+    empty: {
+      login: 'Failed to register. Login must be provided',
+      password: 'Failed to register. Password must be provided',
+    }
+  },
+  login: {
+    common: 'Login error. Access denied',
+    wrongCredentials: 'Wrong credentials. Access denied',
+  },
+  worstScenario: 'Server error'
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+import errors from '../errMessages'
+
 router.get('/', (req, res) => {
   res.status(200).sendFile(path.resolve(__dirname, '../../index.html'))
 })
