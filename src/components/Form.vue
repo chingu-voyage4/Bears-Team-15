3be1c106885022f065bf2dcdd8ed598bef064a6f
svmi3195 <target1983@gmail.com>
2018-03-17 19:16:00 +0200

    add login-register navigation

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,12 +1,33 @@
 <template>
 <div>
-  <div>
-    <router-view/>
-  </div>
+  <h1>{{ greeting }}</h1>
+  <app-nav-tabs :navItems="navItems"></app-nav-tabs>
+  <div><router-view/></div>
 </div>
 </template>
 
 <script>
+import NavTabs from '@/components/NavTabs'
+
+export default {
+  name: "Form",
+  components: {
+    appNavTabs: NavTabs,
+  },
+  data: () => ({
+    greeting: 'Welcome to Bears-15 Cards!',
+    navItems: [
+      {
+        display: 'Login',
+        route: { name: 'login' }
+      },
+      {
+        display: 'Register',
+        route: { name: 'register' }
+      },
+    ]
+  })
+}
 </script>
 
 <style scoped>
