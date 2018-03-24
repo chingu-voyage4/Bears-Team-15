73f5c81dfa241fc9007a79ec8bd72a3427aa6a42
svmi3195 <target1983@gmail.com>
2018-03-24 17:49:01 +0200

    login/register form makes requests to backend

--- src/components/FormLogin.vue
+++ src/components/FormLogin.vue
@@ -1,22 +1,37 @@
 <template>
-<form @submit.prevent="login">
-  <label>Name</label>
-  <input type="text" placeholder="Your login name" v-model="name">
-  <label>Password</label>
-  <input type="password" placeholder="Your password" v-model="password">
-  <button class="btn btn-add" type="submit">Login</button>
-</form>
+<div>
+  <div class="loginError" 
+  v-if="errors && errors.length" 
+  v-for="(error, index) of errors" :key="index">
+    {{error}}
+  </div>
+  <form @submit.prevent="login">
+    <label>Name</label>
+    <input type="text" placeholder="Your login name" v-model="name">
+    <label>Password</label>
+    <input type="password" placeholder="Your password" v-model="password">
+    <button class="btn btn-add" type="submit">Login</button>
+  </form>
+</div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data: () => ({
     name: '',
-    password: ''
+    password: '',
+    errors: []
   }),
   methods: {
     login(){
-      console.log(this.name, this.password)
+      this.errors = []
+      axios.post('/login', { login: this.name, password: this.password })
+        .then(response => { })
+      .catch(e => {
+        this.errors.push(e.response.data)
+      })
     }
   }
 }
--- src/components/FormRegister.vue
+++ src/components/FormRegister.vue
@@ -1,25 +1,40 @@
 <template>
-<form @submit.prevent="register">
-  <label>Email</label>
-  <input  type="text" placeholder="Your email address" v-model="email">
-  <label>Name</label>
-  <input  type="text" placeholder="Your login name" v-model="name">
-  <label>Password</label>
-  <input type="password" placeholder="Your password" v-model="password">
-  <button class="btn btn-add" type="submit">Register</button>
-</form>
+<div>
+  <div class="loginError" 
+    v-if="errors && errors.length" 
+    v-for="(error, index) of errors" :key="index">
+      {{error}}
+    </div>
+  <form @submit.prevent="register">
+    <label>Email</label>
+    <input  type="text" placeholder="Your email address" v-model="email">
+    <label>Name</label>
+    <input  type="text" placeholder="Your login name" v-model="name">
+    <label>Password</label>
+    <input type="password" placeholder="Your password" v-model="password">
+    <button class="btn btn-add" type="submit">Register</button>
+  </form>
+</div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data: () => ({
     email: '',
     name: '',
-    password: ''
+    password: '',
+    errors: []
   }),
   methods: {
     register(){
-      console.log(this.email, this.name, this.password)
+      this.errors = []
+      axios.post('/register', { email: this.email, login: this.name, password: this.password })
+        .then(response => { })
+      .catch(e => {
+        this.errors.push(e.response.data)
+      })
     }
   }
 }
