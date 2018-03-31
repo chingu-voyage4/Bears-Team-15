55aba850070a54cfff1b702b0727509ab9bb3ba2
Anton Tymczenko <tymczenko@protonmail.com>
2018-03-31 13:24:23 +0300

    refactor: Move Dashboard CSS to main place of global CSS

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -35,24 +35,4 @@ export default {
 </script>
 
 <style>
-.dashboard {
-  width: 80%;
-  min-height: 400px;
-  margin: 0 auto;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-}
-
-.deck {
-  width: 140px;
-  height: 200px;
-  margin: 40px;
-  padding: 60px 0;
-  background: rgb(240, 240, 240);
-  border-radius: 10px;
-  box-shadow: 2px 5px 0px rgb(200, 200, 200),
-    4px 10px 0px rgb(180, 180, 180),
-    12px 15px 10px rgba(0, 0, 0, 0.2);
-}
 </style>
