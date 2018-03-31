ea260d75331817f4c7c7104b4a9664e89ee34d34
svmi3195 <target1983@gmail.com>
2018-03-31 22:31:47 +0300

    console log auth token

--- src/components/FormLogin.vue
+++ src/components/FormLogin.vue
@@ -13,11 +13,15 @@ export default {
     formFields: FormFields,
   },
   data: () => ({
+    authorization: ''
   }),
   methods: {
     login(payload) {
       axios.post('/login', payload)
-        .then(response => { })
+        .then(response => {
+            this.authorization = response.headers.authorization.split(' ')[1]
+            console.log(this.authorization) 
+          })
         .catch(e => {
           console.log(e.response.data)
         })
