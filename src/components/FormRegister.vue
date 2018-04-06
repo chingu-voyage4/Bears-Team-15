1ba962eaa9b69f78a995dd09e7e6a3cecf12c440
svmi3195 <target1983@gmail.com>
2018-04-06 23:06:43 +0300

    dont push user on register

--- src/components/FormRegister.vue
+++ src/components/FormRegister.vue
@@ -21,7 +21,7 @@ export default {
         .then(response => { 
           const token = response.headers.authorization
           const user = response.data
-          this.$store.dispatch('processLogin', { user, token} )        
+          this.$store.dispatch('processRegistration', token)        
           this.$store.dispatch('pushNotificationSucc', 'Successfully registered as ' + user.login)
           this.$router.push(this.homeRoute)
          })
