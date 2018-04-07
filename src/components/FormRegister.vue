51426a55c4acd454da085c774834173d6f8b31a9
svmi3195 <target1983@gmail.com>
2018-04-07 14:20:29 +0300

    show notifications with network errors messages

--- src/components/FormRegister.vue
+++ src/components/FormRegister.vue
@@ -27,7 +27,12 @@ export default {
             this.$router.push(this.homeRoute)
           })
           .catch(e => {
-            if(e.response){ this.$store.dispatch('pushNotificationErr', e.response.data) }
+            if(e.response){
+              this.$store.dispatch('pushNotificationErr', e.response.data)
+            }
+            if(e.message){
+              this.$store.dispatch('pushNotificationErr', e.message)
+            }
           })
       }
     },
